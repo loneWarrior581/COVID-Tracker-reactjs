fc7dc486f8dfcc6688668005383203fd6a4b5072
uttkarsh <uttkarshsingh789@gmail.com>
2020-11-18 01:53:26 +0530

    basic project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import {FormControl,MenuItem,Select} from '@material-ui/core'
 
 function App() {
+  const [countries,setCountries]=useState(['USA','UK','INDIA'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <h1>COVID 19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map((country)=>(
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+            {/* <MenuItem value="something">This</MenuItem>
+            <MenuItem value="something">is</MenuItem>
+            <MenuItem value="something">dope</MenuItem>
+            <MenuItem value="something">insane</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+    
+     {/* List of all the components used  */}
+     {/* Headers */}
+     {/* Title +selecting the input dropdown field */}
+
+     {/* Infobox  */}
+     {/* Infobox  */}
+     {/* Infobox  */}
+
+     {/* Table */}
+     {/* Graph  */}
+
+     {/* Maps */}
     </div>
   );
 }
